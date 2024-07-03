cf3cc7ab36ff5180cc2efefad6ac85fda3e4f86e
YashwantTelang-1525 <yeshwantstelang@gmail.com>
2024-07-04 00:36:49 +0530

    Initial commit

---
+++ chessWeb/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { HeaderComponent } from './header/header.component';
+
+export const routes: Routes = [
+    
+    
+];
---
+++ chessWeb/src/app/carousel/carousel.component.ts
@@ -0,0 +1,14 @@
+import { Component, AfterViewInit, ElementRef  } from '@angular/core';
+
+declare var bootstrap: any;
+
+@Component({
+  selector: 'app-carousel',
+  standalone: true,
+  imports: [],
+  templateUrl: './carousel.component.html',
+  styleUrl: './carousel.component.css'
+})
+export class CarouselComponent {
+
+}
